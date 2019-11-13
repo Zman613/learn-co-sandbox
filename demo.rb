49f9f9bf37136985720147461ffd37ac802f10ef
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-13 19:52:32 +0000

    Automatically backed up by Learn

---
+++ demo.rb
@@ -0,0 +1,7 @@
+puts "Hello ruby bla bla #{3 * 9} times!"
+run_code_inside = false
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "code after if...end"
---
+++ learning-comments.rb
@@ -0,0 +1,56 @@
+# run_code_inside = false
+# puts "Code before if...end"
+# if run_code_inside
+#   puts "code inside"
+# end
+# puts "code after if...end"
+
+#chance_of_rain = -98
+# puts "Let's go outside!"
+#if chance_of_rain <= 0.25
+ # puts "Pack a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+ # puts "Pack an umbrella!"
+#else
+ # puts "Stay home and read Tolkien."
+#end
+# puts "Oh, and always wear sunscreen!"
+#this_year = Time.now.year 
+#puts "hey its 2019" if this_year == 2019
+count = 10  
+#while count >= 3 do
+#  puts "HI Mottel!"
+#  count /= 2
+#end
+
+#magic_exit_number = 7
+#count = 0
+#while count < 10 && count != magic_exit_number do
+#  puts "I am the #{count}, I love to count!" # Work
+#  count += 1
+#end
+
+#count = 0 
+#n = 3
+#loop do 
+#  break if count >= n 
+#  puts "I ran."
+#  count += 1 
+#end
+
+#counter = 0 
+#until counter == 20 
+#  puts "The current counter is less than 20."
+#  counter += 1 
+#end
+def say_hello_world_five_times
+  message = "Hello World!"
+  5.times do
+    puts message
+  end
+end 
+
+say_hello_world_five_times
+
+  
+  
